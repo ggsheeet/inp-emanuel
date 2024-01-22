907e05a646e6252398588d849892ba8cfd293d3f
Luis Gzz <ggcryptodoc@gmail.com>
2024-01-21 21:33:19 -0600

    fix: added csp through middleware file + add link to logo

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const cspHeader = `
-  default-src 'self';
-  script-src 'self' 'strict-dynamic';
-  style-src 'self';
-  img-src 'self' blob: data: ${process.env.NEXT_PUBLIC_S3_BUCKET_URL};
-  font-src 'self';
-  object-src 'none';
-  base-uri 'self';
-  form-action 'self';
-  frame-ancestors 'none';
-  block-all-mixed-content;
-  upgrade-insecure-requests;
-`.replace(/\s{2,}/g, ' ').trim()
-
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -21,19 +7,6 @@ const nextConfig = {
         hostname: "inp-emmanuel.s3.us-east-2.amazonaws.com",
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader
-          },
-        ],
-      },
-    ];
   },
 };
 
--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,8 +1,9 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import InpLogo from '/public/inp-main.png'
 import styles from './Nav.module.css'
 import { useFontContext } from '../../lib/context/FontContext'
@@ -14,15 +15,23 @@ const NavBar = () => {
         router.back()
     }
 
+    const currentUrl = usePathname()
+    
+    useEffect(() => {
+        console.log(currentUrl)
+    }, [currentUrl])
+
     const { rale, ruhl } = useFontContext()
 
     return (
         <nav>
             <div className={styles.nav_title_logo}>
-                <div className={`${styles.nav_title} ${ruhl.className}`}>
-                    <p>Iglesia Nacional Presbiteriana</p>
-                    <p>Emanuel Dios con Nostros</p>
-                </div>
+                <Link href='/'>
+                    <div className={`${styles.nav_title} ${ruhl.className}`}>
+                        <p>Iglesia Nacional Presbiteriana</p>
+                        <p>Emanuel Dios con Nostros</p>
+                    </div>
+                </Link>
                 <Image src={InpLogo} alt='INP Logo' width={232} height={325} className={styles.nav_logo} />
             </div>
             <div className={styles.nav_menu}>
