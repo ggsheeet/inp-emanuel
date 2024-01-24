121125a51564b4ea2f9aa2c68e607528a5d7b004
Luis Gzz <ggcryptodoc@gmail.com>
2024-01-24 01:19:12 -0600

    fix: added priority attribute to hero imgs

--- src/components/heroMain/heroImg/HeroImg.tsx
+++ src/components/heroMain/heroImg/HeroImg.tsx
@@ -35,9 +35,9 @@ export const HeroImg = () => {
 
     return (
         <>
-            <Image alt='hero image one' src={HeroImgOne} loading='eager' className={`${styles.mySlides} ${styles.hero_img}`} width={768} height={124} />
-            <Image alt='hero image two' src={HeroImgTwo} loading='eager' className={`${styles.mySlides} ${styles.hero_img}`} width={1600} height={900} />
-            <Image alt='hero image three' src={HeroImgThree} loading='eager' className={`${styles.mySlides} ${styles.hero_img}`} width={2048} height={1536} />
+            <Image alt='hero image one' src={HeroImgOne} loading='eager' priority className={`${styles.mySlides} ${styles.hero_img}`} width={768} height={124} />
+            <Image alt='hero image two' src={HeroImgTwo} loading='eager' priority className={`${styles.mySlides} ${styles.hero_img}`} width={1600} height={900} />
+            <Image alt='hero image three' src={HeroImgThree} loading='eager' priority className={`${styles.mySlides} ${styles.hero_img}`} width={2048} height={1536} />
         </>
     )
 }
