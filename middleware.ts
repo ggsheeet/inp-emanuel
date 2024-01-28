b2bb8d67d6bb543acf6e153029ae3eac25df7d6a
Luis Gzz <ggcryptodoc@gmail.com>
2024-01-28 00:58:51 -0600

    add: nonce to csp in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
-  const cspHeader = `
+	const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+	const cspHeader = `
     default-src 'self';
-    script-src 'self' 'strict-dynamic';
-    style-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data: ${process.env.NEXT_PUBLIC_S3_BUCKET_URL};
     font-src 'self';
     object-src 'none';
@@ -14,28 +14,28 @@ export function middleware(request: NextRequest) {
     frame-ancestors 'none';
     block-all-mixed-content;
     upgrade-insecure-requests;
-`;
-  const contentSecurityPolicyHeaderValue = cspHeader
-    .replace(/\s{2,}/g, " ")
-    .trim();
+`
+	const contentSecurityPolicyHeaderValue = cspHeader
+		.replace(/\s{2,}/g, ' ')
+		.trim()
 
-  const requestHeaders = new Headers(request.headers);
-  requestHeaders.set("x-nonce", nonce);
+	const requestHeaders = new Headers(request.headers)
+	requestHeaders.set('x-nonce', nonce)
 
-  requestHeaders.set(
-    "Content-Security-Policy",
-    contentSecurityPolicyHeaderValue
-  );
+	requestHeaders.set(
+		'Content-Security-Policy',
+		contentSecurityPolicyHeaderValue
+	)
 
-  const response = NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  });
-  response.headers.set(
-    "Content-Security-Policy",
-    contentSecurityPolicyHeaderValue
-  );
+	const response = NextResponse.next({
+		request: {
+			headers: requestHeaders
+		}
+	})
+	response.headers.set(
+		'Content-Security-Policy',
+		contentSecurityPolicyHeaderValue
+	)
 
-  return response;
+	return response
 }
