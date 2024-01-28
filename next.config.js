ea514da068ff16c6c8d0944117fa1061a121193c
Luis Gzz <ggcryptodoc@gmail.com>
2024-01-28 02:00:28 -0600

    fix: migrated csp headers to nextconfig

--- next.config.js
+++ next.config.js
@@ -1,13 +1,39 @@
 /** @type {import('next').NextConfig} */
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: ${process.env.NEXT_PUBLIC_S3_BUCKET_URL};
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "inp-emmanuel.s3.us-east-2.amazonaws.com",
-      },
-    ],
-  },
-};
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'Content-Security-Policy',
+						value: cspHeader.replace(/\n/g, '')
+					}
+				]
+			}
+		]
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'inp-emmanuel.s3.us-east-2.amazonaws.com'
+			}
+		]
+	}
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
