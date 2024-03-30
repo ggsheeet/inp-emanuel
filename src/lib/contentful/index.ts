bb39c4a67c9bbbf7b270bb08762447052df81178
Luis Gzz <ggcryptodoc@gmail.com>
2024-03-30 16:39:06 -0600

    optimized contentful fetch logic

--- src/lib/contentful/index.ts
+++ src/lib/contentful/index.ts
@@ -1,154 +1,108 @@
 import { ItemProps, AssetProps, AuthorProps } from '@/types/globalTypes'
 
-export const getBlogPosts = async () => {
+const fetchEntries = async (contentType: string, limit: number) => {
 	try {
 		const res = await fetch(
-			`https://cdn.contentful.com/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE}/environments/${process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT}/entries?content_type=blog&limit=3&access_token=${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`
+			`https://cdn.contentful.com/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE}/environments/${process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT}/entries?content_type=${contentType}&limit=${limit}&access_token=${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`
 		)
 
 		if (!res.ok) {
-			throw new Error(`Failed to fetch blog posts: ${res.statusText}`)
+			throw new Error(`Failed to fetch ${contentType}: ${res.statusText}`)
 		}
 
-		const data = await res.json()
-		const assetMap = new Map<string, AssetProps>(
-			data.includes.Asset.map((asset: AssetProps) => [asset.sys.id, asset])
-		)
+		return await res.json();
+	} catch (error) {
+		console.error(`Error fetching ${contentType}:`, error)
+		return [];
+	}
+}
 
-		const authorMap = new Map<string, AuthorProps>(
-			data.includes.Entry.map((author: AuthorProps) => [author.sys.id, author])
-		)
+const mapThumbnail = (thumbnailId: string, assetMap: Map<string, AssetProps>) => {
+	const thumbnailAsset = assetMap.get(thumbnailId);
+	return thumbnailAsset ? {
+		description: thumbnailAsset.fields.description,
+		url: thumbnailAsset.fields.file.url,
+		width: thumbnailAsset.fields.file.details.image.width,
+		height: thumbnailAsset.fields.file.details.image.height
+	} : {
+		description: '',
+		url: '',
+		width: 0,
+		height: 0
+	};
+}
 
-		const mappedData = data.items.map((item: ItemProps) => {
-			const { fields, sys } = item
+const mapAuthor = (authorId: string, authorMap: Map<string, AuthorProps>, assetMap: Map<string, AssetProps>) => {
+	const authorInfo = authorMap.get(authorId);
+	if (!authorInfo) return { authorName: '', authorImgInfo: { description: '', url: '', width: 0, height: 0 } };
 
-			const thumbnailId = fields.thumbnail.sys.id
-			const thumbnailAsset = assetMap.get(thumbnailId)
+	const { name, face } = authorInfo.fields;
+	const authorImgId = face.sys.id;
+	const authorImg = assetMap.get(authorImgId);
 
-			let thumbnailInfo = {
-				description: '',
-				url: '',
-				width: 0,
-				height: 0
-			}
+	const authorName = name;
+	const authorImgInfo = authorImg ? {
+		description: authorImg.fields.description,
+		url: authorImg.fields.file.url,
+		width: authorImg.fields.file.details.image.width,
+		height: authorImg.fields.file.details.image.height
+	} : { description: '', url: '', width: 0, height: 0 };
 
-			if (thumbnailAsset) {
-				thumbnailInfo = {
-					description: thumbnailAsset.fields.description,
-					url: thumbnailAsset.fields.file.url,
-					width: thumbnailAsset.fields.file.details.image.width,
-					height: thumbnailAsset.fields.file.details.image.height
-				}
-			}
+	return { authorName, authorImgInfo };
+}
 
-			const authorId = fields.author.sys.id
-			const authorInfo = authorMap.get(authorId)
+export const getBlogPosts = async () => {
+	try {
+		const data = await fetchEntries('blog', 3);
+		const assetMap = new Map<string, AssetProps>(data.includes.Asset.map((asset: AssetProps) => [asset.sys.id, asset]));
+		const authorMap = new Map<string, AuthorProps>(data.includes.Entry.map((author: AuthorProps) => [author.sys.id, author]));
 
-			let authorName = ''
-			let authorImgInfo = {
-				description: '',
-				url: '',
-				width: 0,
-				height: 0
-			}
+		return data.items.map((item: ItemProps) => {
+			const { fields, sys } = item;
+			const { thumbnail, author } = fields;
+			const { createdAt } = sys;
 
-			if (authorInfo) {
-				authorName = authorInfo.fields.name
+			const thumbnailInfo = mapThumbnail(thumbnail.sys.id, assetMap);
+			const { authorName, authorImgInfo } = mapAuthor(author.sys.id, authorMap, assetMap);
 
-				const authorImgId = authorInfo.fields.face.sys.id
-				const authorImg = assetMap.get(authorImgId)
+			const formattedCreatedAt = new Intl.DateTimeFormat('es-ES', { month: 'long', day: 'numeric' }).format(new Date(createdAt));
 
-				if (authorImg) {
-					authorImgInfo = {
-						description: authorImg.fields.description,
-						url: authorImg.fields.file.url,
-						width: authorImg.fields.file.details.image.width,
-						height: authorImg.fields.file.details.image.height
-					}
-				}
-			}
-
-			const createdAt = new Date(sys.createdAt)
-
-			const options: Intl.DateTimeFormatOptions = {
-				month: 'long',
-				day: 'numeric'
-			}
-			const dateFormat = new Intl.DateTimeFormat('es-ES', options)
-
-			const formattedCreatedAt = dateFormat.format(createdAt)
 			return {
 				title: fields.title,
 				description: fields.description,
 				thumbnail: thumbnailInfo,
 				createdAt: formattedCreatedAt,
-				authorName: authorName,
+				authorName,
 				authorImg: authorImgInfo
-			}
-		})
-		return mappedData
+			};
+		});
 	} catch (error) {
-		console.error('Error fetching blog posts:', error)
-		return []
+		console.error('Error fetching blog posts:', error);
+		return [];
 	}
 }
 
 export const getEvents = async () => {
 	try {
-		const res = await fetch(
-			`https://cdn.contentful.com/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE}/environments/${process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT}/entries?content_type=events&limit=3&order=-sys.createdAt&access_token=${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`
-		)
+		const data = await fetchEntries('events', 3);
+		const assetMap = new Map<string, AssetProps>(data.includes.Asset.map((asset: AssetProps) => [asset.sys.id, asset]));
 
-		if (!res.ok) {
-			throw new Error(`Failed to fetch events: ${res.statusText}`)
-		}
+		return data.items.map((item: ItemProps) => {
+			const { fields } = item;
+			const { thumbnail, startTime, endTime } = fields;
 
-		const data = await res.json()
-		const assetMap = new Map<string, AssetProps>(
-			data.includes.Asset.map((asset: AssetProps) => [asset.sys.id, asset])
-		)
+			const thumbnailInfo = mapThumbnail(thumbnail.sys.id, assetMap);
 
-		const mappedData = data.items.map((item: ItemProps) => {
-			const { fields } = item
+			const options: Intl.DateTimeFormatOptions = { month: 'long', day: 'numeric' };
+			const dateFormat = new Intl.DateTimeFormat('es-ES', options);
+			const timeFormat = new Intl.DateTimeFormat('es-ES', { hour: 'numeric', minute: 'numeric', hour12: true });
 
-			const thumbnailId = fields.thumbnail.sys.id
-			const thumbnailAsset = assetMap.get(thumbnailId)
-
-			let thumbnailInfo = {
-				description: '',
-				url: '',
-				width: 0,
-				height: 0
-			}
-
-			if (thumbnailAsset) {
-				thumbnailInfo = {
-					description: thumbnailAsset.fields.description,
-					url: thumbnailAsset.fields.file.url,
-					width: thumbnailAsset.fields.file.details.image.width,
-					height: thumbnailAsset.fields.file.details.image.height
-				}
-			}
-
-			const startDate = new Date(fields.startTime)
-			const endDate = new Date(fields.endTime)
-
-			const options: Intl.DateTimeFormatOptions = {
-				month: 'long',
-				day: 'numeric'
-			}
-
-			const dateFormat = new Intl.DateTimeFormat('es-ES', options)
-			const timeFormat = new Intl.DateTimeFormat('es-ES', {
-				hour: 'numeric',
-				minute: 'numeric',
-				hour12: true
-			})
-
-			const formattedStartDate = dateFormat.format(startDate)
-			const formattedStartTime = timeFormat.format(startDate)
-			const formattedEndDate = dateFormat.format(endDate)
-			const formattedEndTime = timeFormat.format(endDate)
+			const startDate = new Date(startTime);
+			const endDate = new Date(endTime);
+			const formattedStartDate = dateFormat.format(startDate);
+			const formattedStartTime = timeFormat.format(startDate);
+			const formattedEndDate = dateFormat.format(endDate);
+			const formattedEndTime = timeFormat.format(endDate);
 
 			return {
 				title: fields.title,
@@ -159,11 +113,10 @@ export const getEvents = async () => {
 				description: fields.description,
 				location: fields.location,
 				thumbnail: thumbnailInfo
-			}
-		})
-		return mappedData
+			};
+		});
 	} catch (error) {
-		console.error('Error fetching events:', error)
-		return []
+		console.error('Error fetching events:', error);
+		return [];
 	}
 }
